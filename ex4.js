a8c714f4416d099a62e9f16e75575eb4637fc32a
eucaions <caiotm.nascimento@gmail.com>
2025-01-13 14:21:54 -0300

    Update ex4
    adição do console.log

--- ex4.js
+++ ex4.js
@@ -4,4 +4,6 @@ while (isNaN(real)) {
     real = parseFloat(prompt("Valor inválido. Digite o valor em Reais (R$):")).toFixed(2);
 }
 alert(`O valor de R$ ${real} convertido para Euro fica em ${(real/6.10).toFixed(2)}`);
+console.log(`O valor de R$ ${real} convertido para Euro fica em ${(real/6.10).toFixed(2)}`);
 alert(`O valor de R$ ${real} convertido para Dólar fica em ${(real/5.70).toFixed(2)}`);
+console.log(`O valor de R$ ${real} convertido para Dólar fica em ${(real/5.70).toFixed(2)}`);
